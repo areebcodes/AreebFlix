ee55d062b65f8ee464e5d0375662048ea3d252e9
Areeb Khan <makhan9075@gmail.com>
2026-03-20 00:28:32 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
-import ToDoList from "./ToDoList";
+import "./css/App.css";
+import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
+import { MovieProvider } from "./contexts/MovieContext.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
- 
-    return(
-        <>
-            <ToDoList/>
-        </>
-    );
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
